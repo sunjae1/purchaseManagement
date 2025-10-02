3e4d87fecbdacdcdda0c1f17a5b3302141da51bf
sunjae1 <mk35plkr@naver.com>
2025-10-02 17:59:47 +0900

    adding itemrepository

--- src/main/java/com/example/purchaseManagement/repository/ItemRepository.java
+++ src/main/java/com/example/purchaseManagement/repository/ItemRepository.java
@@ -1,11 +1,27 @@
 package com.example.purchaseManagement.repository;
 
+import com.example.purchaseManagement.domain.Item;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 @Repository
+@Transactional(readOnly = true)
 public class ItemRepository {
+
+    @PersistenceContext
+    private EntityManager em;
+
+    //저장 (persist -> INSERT)
+    public void save(Item item) {
+        em.persist(item);
+    }
+
+    //수정 (merge -> UPDATE)
+    public Item update(Item item) {
+        return em.merge(item);
+    }
 
 
 }
